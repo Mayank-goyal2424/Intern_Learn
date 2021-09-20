3488c7bea4d27dfe872b3c73dd63ab26a665e74e
Mayank Goyal <mayankgoyal2424@gmail.com>
2021-09-20 23:43:25 +0530

    initial commit

---
+++ Test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //int n=10;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+        System.out.print(n);
+		
+		float f=sc.nextFloat();
+        System.out.print(f);
+		//concatenation
+		   System.out.print("Addition of"+n+" and "+  f+" is: "+f+n);
+	}
+
+}
